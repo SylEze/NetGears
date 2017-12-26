4974a53778385b958ac3c371179ec3b8de9e9787
Syl Eze <syleze.contact@gmail.com>
2017-12-26 01:09:58 +0100

    add account entity

--- NetGears.ORM/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NetGears.ORM
-{
-    public class Class1
-    {
-    }
-}
---
+++ NetGears.ORM/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetGears.ORM.Entities
+{
+    public class Account
+    {
+        public long Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
