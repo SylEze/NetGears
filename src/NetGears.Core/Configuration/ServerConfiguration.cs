f4bd10a8a3ca524efdb34bec132541463cdb0d4c
Syl Eze <syleze.contact@gmail.com>
2018-01-21 22:30:58 +0100

    add ServerConfiguration class

---
+++ src/NetGears.Core/Configuration/ServerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace NetGears.Core.Configuration
+{
+    [DataContract]
+    public class ServerConfiguration
+    {
+        [DataMember(Name = "port")]
+        public ushort Port { get; set; }
+
+        [DataMember(Name = "host")]
+        public string Host { get; set; }
+    }
+}
