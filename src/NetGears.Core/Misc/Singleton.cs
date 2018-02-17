8a1f8e228fe989a5f8894e9b468f71390c0e653c
SylEze <syleze.contact@gmail.com>
2018-02-17 21:39:33 +0100

    add standard login configuration

---
+++ src/NetGears.Core/Misc/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace NetGears.Core.Misc
+{
+    public class Singleton<T> where T : class, new()
+    {
+        private static T _instance;
+
+        private static readonly object _bolt = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_bolt)
+                {
+                    _instance = new T();
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ src/NetGears.Login/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+using NetGears.Core.Configuration;
+using NetGears.Core.Logger;
+
+namespace NetGears.Login
+{
+    internal static class Program
+    {
+        private static ServerConfiguration _serverConfiguration;
+        
+        private static void Main(string[] args)
+        {
+            Logger.Initialize(typeof(Program));
+            _serverConfiguration = ConfigurationLoader.Instance.Load<ServerConfiguration>("login.json");
+
+            Logger.Info("Login server started");
+
+            ConfigurationLoader.Instance.Save<ServerConfiguration>(_serverConfiguration, "login.json");
+        }
+    }
+}
