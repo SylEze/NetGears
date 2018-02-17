ebb718e5f8f1d061471cbf658b5d91bd88e4ab7a
Syl Eze <syleze.contact@gmail.com>
2018-02-17 22:27:29 +0100

    db context added

---
+++ src/NetGears.ORM/NetGearsContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using NetGears.ORM.Entities;
+
+namespace NetGears.ORM
+{
+    public class NetGearsContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=netgears;Integrated Security=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
