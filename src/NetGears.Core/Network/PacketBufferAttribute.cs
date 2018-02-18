448adce7563d99262b1c9cf6bae97e6f307b13dc
Syl Eze <syleze.contact@gmail.com>
2018-02-18 23:26:44 +0100

    create packet buffer attribute to deserialize buffers to packets

---
+++ src/NetGears.Core/Network/PacketBufferAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetGears.Core.Network
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PacketBufferAttribute : Attribute
+    {
+        public ushort Length { get; set; }
+
+        public ushort BeginPosition { get; set; }
+
+        public byte Delimitor { get; set; }
+
+        public PacketBufferAttribute(ushort length, ushort beginPosition, byte delimitor)
+        {
+            Length = length;
+            BeginPosition = beginPosition;
+            Delimitor = delimitor;
+        }
+    }
+}
