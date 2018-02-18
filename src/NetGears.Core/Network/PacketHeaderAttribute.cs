fcddc3f67a034f8ade2f7b87e0f828d78d58ca73
Syl Eze <syleze.contact@gmail.com>
2018-02-18 23:01:52 +0100

    create a new header attribute for packets and handler methods retrieving

---
+++ src/NetGears.Core/Network/PacketHeaderAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetGears.Core.Network
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class PacketHeaderAttribute : Attribute
+    {
+        public object Header { get; }
+
+        public PacketHeaderAttribute(object header)
+        {
+            Header = header;
+        }
+    }
+}
