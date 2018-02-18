76f53420e2129e1cbfdec7882705eb2a966aea20
Syl Eze <syleze.contact@gmail.com>
2018-02-18 16:56:12 +0100

    basic database configuration added

--- src/NetGears.Core/Configuration/DatabaseConfiguration.cs
+++ src/NetGears.Core/Configuration/DatabaseConfiguration.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Runtime.Serialization;
 using System.Text;
-using Newtonsoft.Json;
 
 namespace NetGears.Core.Configuration
 {
     [DataContract]
-    public class ServerConfiguration
+    public class DatabaseConfiguration
     {
         [DataMember(Name = "host")]
         public string Host { get; set; }
 
-        [DataMember(Name = "port")]
-        public ushort Port { get; set; }
+        [DataMember(Name = "databaseName")]
+        public string DatabaseName { get; set; }
     }
 }
