aa8a257b132e3641d54e6ccf762ad4e332285ee2
Syl Eze <syleze.contact@gmail.com>
2018-02-18 16:56:48 +0100

    add DatabaseConfiguration inside NetGearsContext

--- src/NetGears.ORM/NetGearsContext.cs
+++ src/NetGears.ORM/NetGearsContext.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using NetGears.Core.Configuration;
 using NetGears.ORM.Entities;
 
 namespace NetGears.ORM
 {
     public class NetGearsContext : DbContext
     {
+        private const string DatabaseConfigurationPath = "database.json";
+
         public DbSet<Account> Accounts { get; set; }
+
+        public DatabaseConfiguration DatabaseConfiguration { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=netgears;Integrated Security=true;");
+            DatabaseConfiguration = ConfigurationLoader.Instance.Load<DatabaseConfiguration>(DatabaseConfigurationPath);
+
+            optionsBuilder.UseSqlServer($"Data Source={DatabaseConfiguration.Host};Initial Catalog={DatabaseConfiguration.DatabaseName};Integrated Security=true;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
