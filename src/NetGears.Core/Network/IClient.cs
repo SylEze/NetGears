c9fd5f978793d2b780ce0413bbf2fa5c2adddaaf
Syl Eze <syleze.contact@gmail.com>
2018-02-20 09:48:07 +0100

    add interfaces for clients and packets

---
+++ src/NetGears.Core/Network/IClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetGears.Core.Network
+{
+    public interface IClient
+    {
+        Guid Id { get; }
+
+        void Disconnect();
+    }
+}
---
+++ src/NetGears.Core/Network/IPacket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetGears.Core.Network
+{
+    public interface IPacket
+    {
+        void Deserialize(byte[] buffer);
+    }
+}
