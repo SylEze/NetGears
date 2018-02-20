4d69c9b2b8324d45b72d86f5004cae717d2f0805
Syl Eze <syleze.contact@gmail.com>
2018-02-20 17:58:38 +0100

    add get sub array extension method for arrays

---
+++ src/NetGears.Core/Extensions/ArrayExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace NetGears.Core.Extensions
+{
+    public static class ArrayExtension
+    {
+        public static T[] GetSubArray<T>(this T[] data, int index, int length)
+        {
+            var result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+    }
+}
