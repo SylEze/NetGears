559dcf0240370efb69f04b7d233b23b04baeb4c5
Syl Eze <syleze.contact@gmail.com>
2018-03-27 08:39:01 +0200

    add generic repository pattern

---
+++ src/NetGears.Database/Repositories/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NetGears.Database.Entities;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
+
+namespace NetGears.Database.Repositories
+{
+    public abstract class Repository<TDatabaseContext, TEntity> : IRepository<TEntity>
+        where TDatabaseContext : DbContext
+        where TEntity : class, IEntity
+    {
+        protected TDatabaseContext _context { get; }
+
+        protected Repository(TDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public TEntity Get(long id)
+        {
+            return _context.Set<TEntity>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public TEntity Get(Func<TEntity, bool> predicate)
+        {
+            return _context.Set<TEntity>().FirstOrDefault(predicate);
+        }
+
+        public IEnumerable<TEntity> GetAll(Func<TEntity, bool> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate);
+        }
+
+        public int Count()
+        {
+            return _context.Set<TEntity>().Count();
+        }
+
+        public int Count(Func<TEntity, bool> predicate)
+        {
+            return _context.Set<TEntity>().Count(predicate);
+        }
+    }
+}
