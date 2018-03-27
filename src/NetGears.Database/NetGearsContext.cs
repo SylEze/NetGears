e857d4cca0f5b0a039b3698ec491187e6f71c1c2
Syl Eze <syleze.contact@gmail.com>
2018-03-27 09:04:53 +0200

    add account repository

--- src/NetGears.Database/NetGearsContext.cs
+++ src/NetGears.Database/NetGearsContext.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using NetGears.Core.Configuration;
+using NetGears.Core.Logger;
 using NetGears.Database.Entities;
+using NetGears.Database.Repositories;
 
 namespace NetGears.Database
 {
@@ -9,14 +12,37 @@ namespace NetGears.Database
         private const string DatabaseConfigurationPath = "database.json";
 
         public DbSet<Account> Accounts { get; set; }
+        
+        public IRepository<Account> AccountRepository { get; }
 
-        public DatabaseConfiguration DatabaseConfiguration { get; set; }
+        public DatabaseConfiguration DatabaseConfiguration { get; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public NetGearsContext()
         {
             DatabaseConfiguration = ConfigurationLoader.Instance.Load<DatabaseConfiguration>(DatabaseConfigurationPath);
 
-            optionsBuilder.UseSqlServer($"Data Source={DatabaseConfiguration.Host};Initial Catalog={DatabaseConfiguration.DatabaseName};Integrated Security=true;");
+            AccountRepository = new AccountRepository(this);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            switch (DatabaseConfiguration.DatabaseProvider)
+            {
+                case DatabaseProvider.SQLServer:
+                    optionsBuilder.UseSqlServer(
+                        $"Data Source={DatabaseConfiguration.Host};Initial Catalog={DatabaseConfiguration.DatabaseName};Integrated Security=true;");
+                    break;
+                case DatabaseProvider.MySQL:
+                    optionsBuilder.UseMySql(
+                        $"Server={DatabaseConfiguration.Host};Database={DatabaseConfiguration.DatabaseName};" +
+                        $"Uid={DatabaseConfiguration.User};Pwd={DatabaseConfiguration.Password};");
+                    break;
+                default:
+                    throw new NotImplementedException("The selected database provider is not yet implemented " +
+                                                      "or does not exist. Please change your database.json");
+            }
+
+            Logger.Info("Database context configured");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
