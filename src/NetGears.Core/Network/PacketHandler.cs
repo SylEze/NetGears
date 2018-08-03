ee951cc3d99a588c45bd70403ccc53911529edc6
Syl Eze <syleze.contact@gmail.com>
2018-08-03 23:03:38 +0200

    core: improve PacketHandler
    TPacket has to inherit from IPacket

--- src/NetGears.Core/Network/PacketHandler.cs
+++ src/NetGears.Core/Network/PacketHandler.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Reflection.Emit;
 
 namespace NetGears.Core.Network
 {
-    public static class PacketHandler<TClient, TPacketBase>
-        where TClient : class
-        where TPacketBase : class
+    public class PacketHandler<TClient, TPacket> where TPacket : IPacket
     {
+        private static readonly Logger.Logger Logger = NetGears.Core.Logger.Logger.GetLogger<PacketHandler<TClient, TPacket>>();
+        
         /// <summary>
         /// Dictionary which contains method references to invoke
         /// Each key corresponds to a packet header defined by the user
         /// </summary>
         private static readonly Dictionary<Type, Delegate> MethodReferences = new Dictionary<Type, Delegate>();
 
-        public static void LoadFrom<T>()
+        private static readonly Dictionary<PacketHeaderAttribute, Type> PacketTypes =
+            new Dictionary<PacketHeaderAttribute, Type>();
+
+        public static void LoadPackets(PacketServerType serverType)
         {
-            IEnumerable<MethodInfo> methods = typeof(T).GetMethods()
-                .Where(m => m.IsPublic && m.IsStatic);
+            var packets = typeof(TPacket)
+                .Assembly
+                .GetTypes()
+                .Where(m => m.GetCustomAttributes(typeof(PacketHeaderAttribute), false)
+                .Any(x => ((PacketHeaderAttribute) x).ServerType == serverType && 
+                          ((PacketHeaderAttribute) x).SenderType == PacketSenderType.SERVER))
+                .ToList();
+
+            foreach (var packet in packets)
+            {
+                var idAttribute = packet.GetCustomAttribute(typeof(PacketHeaderAttribute));
+
+                PacketTypes.Add(idAttribute as PacketHeaderAttribute, packet);
+
+                Logger.Debug($"{packet.Name} packet registered.");
+            }
+        }
+
+        public static void LoadHandlers<THandler>()
+        {
+            var methods = typeof(THandler).GetTypeInfo().DeclaredMethods;
 
             foreach (var method in methods)
             {
@@ -32,14 +53,19 @@ namespace NetGears.Core.Network
                 {
                     throw new ArgumentException($"There must be two arguments in handler method: ${method.Name}");
                 }
+
                 if (parameters[0].Type != typeof(TClient))
                 {
-                    throw new ArgumentException($"${method.Name} First argument must be of type: ${typeof(TClient)}\tWas of type: ${parameters[0].Type}");
+                    throw new ArgumentException(
+                        $"${method.Name} First argument must be of type: ${typeof(TClient)}\tWas of type: ${parameters[0].Type}");
                 }
-                if (parameters[1].Type.BaseType != typeof(TPacketBase))
+
+                if (parameters[1].Type.BaseType != typeof(TPacket))
                 {
-                    throw new ArgumentException($"${method.Name} Second argument must derived from : ${typeof(TPacketBase)}\tWas of type: ${parameters[1].Type}");
+                    throw new ArgumentException(
+                        $"${method.Name} Second argument must derived from : ${typeof(TPacket)}\tWas of type: ${parameters[1].Type}");
                 }
+
                 if (!method.IsStatic)
                 {
                     throw new ArgumentException("The provided method must be static.", "method");
@@ -52,16 +78,20 @@ namespace NetGears.Core.Network
 
                 var deleg = method.CreateDelegate(Expression.GetDelegateType(
                     (from parameter in method.GetParameters() select parameter.ParameterType)
-                    .Concat(new[] { method.ReturnType })
+                    .Concat(new[] {method.ReturnType})
                     .ToArray()));
 
                 MethodReferences.Add(parameters[1].Type, deleg);
+
+                Logger.Debug($"{method.Name} method registered.");
             }
         }
 
-        public static void ExecuteHandler(TClient invoker, TPacketBase packet)
+
+        public static void ExecuteHandler(TClient client, TPacket packet)
         {
-            MethodReferences[packet.GetType()].DynamicInvoke(invoker, packet);
+
+            MethodReferences[packet.GetType()].DynamicInvoke(client, packet);
         }
     }
 }
