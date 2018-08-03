fabab2f76af53f551e1cf0a9e2cefa732162c59a
Syl Eze <syleze.contact@gmail.com>
2018-08-03 23:47:17 +0200

    login: add LoginConfiguration

---
+++ src/NetGears.Login/Misc/LoginConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace NetGears.Login.Misc
+{
+    [DataContract]
+    public struct LoginConfiguration
+    {
+        [DataMember(Name = "host")]
+        public string Host { get; set; }
+
+        [DataMember(Name = "port")]
+        public ushort Port { get; set; }
+        
+        [DataMember(Name = "max_connections")]
+        public uint MaxConnections { get; set; }
+        
+        [DataMember(Name = "cluster_host")]
+        public string ClusterHost { get; set; }
+        
+        [DataMember(Name = "cluster_port")]
+        public ushort ClusterPort { get; set; }
+        
+        [DataMember(Name = "cluster_token")]
+        public string ClusterToken { get; set; }
+    }
+}
