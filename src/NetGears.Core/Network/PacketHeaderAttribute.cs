3ffab24b402bd767323ec301202860e5ecdeec74
Syl Eze <syleze.contact@gmail.com>
2018-08-03 23:04:47 +0200

    core: improve PacketHeaderAttribute
    add SenderType property
    add ServerType property

---
+++ src/NetGears.Core/Network/PacketHeaderAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NetGears.Core.Network
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class PacketHeaderAttribute : Attribute
+    {
+        public short Id { get; }
+
+        public int Length { get; }
+        
+        public PacketSenderType SenderType { get; }
+        
+        public PacketServerType ServerType { get; }
+
+        public PacketHeaderAttribute(
+            short id, int length, PacketSenderType senderType, PacketServerType serverType)
+        {
+            Id = id;
+            Length = length;
+            SenderType = senderType;
+            ServerType = serverType;
+        }
+    }
+}
