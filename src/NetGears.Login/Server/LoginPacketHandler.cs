38699428e18562098e7feb97be3d7d2b364e2f73
Syl Eze <syleze.contact@gmail.com>
2018-08-04 00:02:17 +0200

    login: fix compilation

--- src/NetGears.Login/Server/LoginPacketHandler.cs
+++ src/NetGears.Login/Server/LoginPacketHandler.cs
@@ -1,5 +1,4 @@
 ﻿using NetGears.Core.Logger;
-using NetGears.Database;
 using NetGears.Game.Packet.Login.Client;
 
 namespace NetGears.Login.Server
@@ -16,17 +15,6 @@ namespace NetGears.Login.Server
         public static void CheckUserCredentials(LoginClient client, AccountPacket packet)
         {
             Logger.Info($"account {packet?.Username} {packet?.Password}");
-
-            using (var context = DatabaseFactory.GetNetGearsContext())
-            {
-                var account = context.AccountRepository.Get(x =>
-                    x.Username == packet?.Username && x.Password == packet?.Password);
-
-                if (account == null)
-                {
-                    Logger.Info($"{client.Id} bad credentials");
-                }
-            }
         }
     }
 }
