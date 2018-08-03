3c9b2164487701291a51462bc750e92593bc8801
Syl Eze <syleze.contact@gmail.com>
2018-08-03 23:45:57 +0200

    game: replace VersionPacket

---
+++ src/NetGears.Game/Packet/Login/Client/VersionPacket.cs
@@ -0,0 +1,24 @@
+﻿using NetGears.Core.Network;
+using NetGears.Game.Enum;
+
+namespace NetGears.Game.Packet.Login.Client
+{
+    [PacketHeader((short)LoginPacketId.VERSION_PACKET, 44, PacketSenderType.CLIENT, PacketServerType.LOGIN)]
+    public class VersionPacket : PacketBase
+    {
+        public string StreetGearsVersion { get; set; }
+        
+        public string Lang { get; set; }
+
+        public override void Deserialize(byte[] buffer)
+        {
+            StreetGearsVersion = "StreetGearsVersion";
+            Lang = "Lang";
+        }
+
+        public override byte[] Serialize()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
