d55613073ea5c939d75142552c4db1ed32cf5dfc
Syl Eze <syleze.contact@gmail.com>
2018-08-05 12:04:31 +0200

    game: add AccountRank enum

---
+++ src/NetGears.Game/Enum/AccountRank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetGears.Game.Enum
+{
+    public enum AccountRank
+    {
+        BANNED = -1,
+        NONE,
+        GM,
+        ADMIN
+    }
+}
