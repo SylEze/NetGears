008eb09b589a15f7b41a15162e04a7f476fd82f5
Syl Eze <syleze.contact@gmail.com>
2018-08-05 12:04:48 +0200

    game: add AccountDto

---
+++ src/NetGears.Game/Data/TransferObject/AccountDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using NetGears.Game.Enum;
+
+namespace NetGears.Game.Data.TransferObject
+{
+    public class AccountDto
+    {
+        public uint Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public AccountRank Rank { get; set; }
+
+        public DateTime LastConnectionDate { get; set; }
+
+        public IPAddress LastConnectionIp { get; set; }
+
+        public string RegistrationEmail { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public IPAddress RegistrationIp { get; set; }
+    }
+}
