972b7e22e05c859396912ab66c4eb2555315da1c
Syl Eze <syleze.contact@gmail.com>
2018-08-31 12:00:12 +0200

    Add IAccountDal

---
+++ src/NetGears.Game/Data/AccessLayer/IAccountDal.cs
@@ -0,0 +1,9 @@
+﻿using NetGears.Game.Data.TransferObject;
+
+namespace NetGears.Game.Data.AccessLayer
+{
+    public interface IAccountDal
+    {
+        AccountDto GetByUsername(string username);
+    }
+}
