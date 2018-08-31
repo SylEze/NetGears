c4e796ced1a6a5c1c11cbebaf7fc9cec82b40fb0
Syl Eze <syleze.contact@gmail.com>
2018-08-31 17:41:17 +0200

    Add AccountDal

---
+++ src/NetGears.Database/DataAccessLayer/AccountDal.cs
@@ -0,0 +1,13 @@
+﻿using NetGears.Game.Data.AccessLayer;
+using NetGears.Game.Data.TransferObject;
+
+namespace NetGears.Database.DataAccessLayer
+{
+    public class AccountDal : GenericDal<AccountDto>, IAccountDal
+    {
+        public AccountDto GetByUsername(string username)
+        {
+            return Get(x => x.Username == username);
+        }
+    }
+}
