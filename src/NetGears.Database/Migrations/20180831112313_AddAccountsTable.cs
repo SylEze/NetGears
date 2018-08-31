d14ec492bb933c376f9d28eefcf5046013122676
Syl Eze <syleze.contact@gmail.com>
2018-08-31 17:43:34 +0200

    Migration: AddAccountsTable

---
+++ src/NetGears.Database/Migrations/20180831112313_AddAccountsTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetGears.Database.Migrations
+{
+    public partial class AddAccountsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Rank = table.Column<int>(nullable: false),
+                    LastConnectionDate = table.Column<DateTime>(nullable: false),
+                    LastConnectionIp = table.Column<string>(nullable: true),
+                    RegistrationEmail = table.Column<string>(nullable: true),
+                    RegistrationDate = table.Column<DateTime>(nullable: false),
+                    RegistrationIp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
