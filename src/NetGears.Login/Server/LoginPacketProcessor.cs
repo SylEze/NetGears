25ae44eb32d4ea969c349ac05acdf22ac2697424
Syl Eze <syleze.contact@gmail.com>
2018-08-31 12:01:10 +0200

    Add LoginPacketProcessor which reads input client packets

---
+++ src/NetGears.Login/Server/LoginPacketProcessor.cs
@@ -0,0 +1,28 @@
+﻿using Ether.Network.Packets;
+using System;
+
+using NetGears.Core.Logger;
+
+namespace NetGears.Login.Server
+{
+    public class LoginPacketProcessor : IPacketProcessor
+    {
+        public static readonly Logger Logger = Logger.GetLogger<LoginPacketProcessor>();
+
+        public int HeaderSize => 5;
+
+        public bool IncludeHeader => true;
+
+        public INetPacketStream CreatePacket(byte[] buffer)
+        {
+            return new NetPacket(buffer);
+        }
+
+        public int GetMessageLength(byte[] buffer)
+        {
+            var length = BitConverter.ToInt16(buffer, 0);
+
+            return length;
+        }
+    }
+}
